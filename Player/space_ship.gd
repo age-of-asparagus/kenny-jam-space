f547d7ba89c77dd71eca5e7587e993e08e787410
kamrancouture <65361792+kamrancouture@users.noreply.github.com>
2023-07-22 18:28:27 -0700

    rotate sound + sprite

--- Player/space_ship.gd
+++ Player/space_ship.gd
@@ -11,29 +11,29 @@ export var rotation_speed = 0.05
 
 
 func _ready():
-	$AudioStreamPlayer.playing = true
-
+	$Move_sound.playing = true
+	$Rotate_sound.playing = true
 
 func _physics_process(delta):
 	
 	
-	if Input.is_action_just_pressed("boost") and not boosting and Global.boosts_available > 0:
+	if Input.is_action_just_pressed("boost") and not boosting and Global.boosts_available > 0 and Global.fuel > 0:
 		$AnimationPlayer.play("boost")
 		boosting = true
 		Global.boosts_available -= 1
 		boost_vector = -transform.y
-		$AudioStreamPlayer.stream_paused = false
+		$Move_sound.stream_paused = false
 		$boost_timer.start()
 
 	
-	if Input.is_action_pressed("move") and not boosting:
+	if Input.is_action_pressed("move") and not boosting and Global.fuel > 0:
 		Global.fuel -= 0.01
 		velocity += -transform.y * acceleration
-		$AudioStreamPlayer.stream_paused = false
+		$Move_sound.stream_paused = false
 		$Sprite/Fire.play("moving")
 		$Sprite/Fire.scale = Vector2(1.5,1.5)
 	elif boosting:
-		Global.fuel -= 0.03
+		Global.fuel -= 0.05
 		velocity += -transform.y * acceleration * boost
 		$Sprite/Fire.scale = Vector2(2,2)
 		$Sprite/Fire.play("boosting")
@@ -41,7 +41,7 @@ func _physics_process(delta):
 	else:
 		$Sprite/Fire.scale = Vector2(1.5,1.5)
 		$Sprite/Fire.play("idle")
-		$AudioStreamPlayer.stream_paused = true
+		$Move_sound.stream_paused = true
 		
 		
 	
@@ -49,6 +49,19 @@ func _physics_process(delta):
 	
 	
 	var rotation_dir = Input.get_axis("rotate_left", "rotate_right")
+	
+	if rotation_dir > 0:
+		$Right_booster.play("idle")
+		$Left_booster.play("boost")
+		$Rotate_sound.stream_paused = false
+	elif rotation_dir < 0:
+		$Rotate_sound.stream_paused = false
+		$Left_booster.play("idle")
+		$Right_booster.play("boost")
+	else:
+		$Rotate_sound.stream_paused = true
+		$Right_booster.play("idle")
+		$Left_booster.play("idle")
 	rotate(rotation_dir * rotation_speed)
 	
 	move_and_slide(velocity)
